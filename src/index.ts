a9213ab755f56ac92ea7104d0d4167e5d08c3d91
Andy <andycheung83a@hotmail.com>
2024-09-21 19:11:42 +0200

    Refactor taskRoutes.ts: Add CRUD operations for tasks

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,9 @@ import express from 'express';
 import mongoose from 'mongoose';
 import userRoutes from './routes/userRoutes';
 import taskRoutes from './routes/taskRoutes';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -11,13 +14,20 @@ app.use(express.json());
 app.use('/users', userRoutes);
 app.use('/tasks', taskRoutes);
 
+// Lägg till denna rotväg
+app.get('/', (req, res) => {
+	res.send('Welcome to the Trullo API');
+});
+
+// Använd miljövariabel för MongoDB-anslutningssträngen
+const mongoURI = process.env.MONGO_URI;
+
+if (!mongoURI) {
+	throw new Error('MONGO_URI is not defined in the environment variables');
+}
+
 mongoose
-	.connect('mongodb://localhost:27017/trullo', {
-		useNewUrlParser: true,
-		useUnifiedTopology: true,
-		useCreateIndex: true,
-		useFindAndModify: false,
-	})
+	.connect(mongoURI)
 	.then(() => {
 		console.log('Connected to MongoDB');
 		app.listen(port, () => {
