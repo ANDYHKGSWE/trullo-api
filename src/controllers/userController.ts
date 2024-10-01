5bad9143c9ab3375fb792c40ee72d21caf866e2d
Andy <andycheung83a@hotmail.com>
2024-10-01 17:02:56 +0200

    Refactor userController.ts and tsconfig.json

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,9 +1,10 @@
 import { Request, Response } from 'express';
-import { User } from '../models/user';
+import { User } from '/path/to/models/user';
 
 export const createUser = async (req: Request, res: Response) => {
 	try {
-		const user = new User(req.body);
+		const { name, email, password, role } = req.body;
+		const user = new User({ name, email, password, role });
 		await user.save();
 		res.status(201).send(user);
 	} catch (error) {
@@ -22,12 +23,15 @@ export const getUsers = async (req: Request, res: Response) => {
 
 export const updateUser = async (req: Request, res: Response) => {
 	try {
-		const user = await User.findByIdAndUpdate(req.params.id, req.body, {
-			new: true,
-			runValidators: true,
-		});
+		const { id } = req.params;
+		const { name, email, password, role } = req.body;
+		const user = await User.findByIdAndUpdate(
+			id,
+			{ name, email, password, role },
+			{ new: true }
+		);
 		if (!user) {
-			return res.status(404).send();
+			return res.status(404).send('User not found');
 		}
 		res.status(200).send(user);
 	} catch (error) {
@@ -37,11 +41,12 @@ export const updateUser = async (req: Request, res: Response) => {
 
 export const deleteUser = async (req: Request, res: Response) => {
 	try {
-		const user = await User.findByIdAndDelete(req.params.id);
+		const { id } = req.params;
+		const user = await User.findByIdAndDelete(id);
 		if (!user) {
-			return res.status(404).send();
+			return res.status(404).send('User not found');
 		}
-		res.status(200).send(user);
+		res.status(200).send('User deleted');
 	} catch (error) {
 		res.status(500).send(error);
 	}
